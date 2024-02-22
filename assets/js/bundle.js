5fabf10f1b6d5dc74425973bf6306a7eec4c4baf
Christian Stussak <christian.stussak@imaginary.org>
2024-02-22 13:58:33 +0100

    Compute the expected values of different strategries deterministically
    Previously, the expected outcomes of the different strategies have been
    computed by a Monte-Carlo simulation, but this takes many iterations to
    collect enough data points to resemble the random distribution.
    The computation blocks the event queue, which results in delayed and
    stuttering animations at the end of each game.
    
    This patch replaces the Monte-Carlo simulation with a deterministic
    approach whose complexity is at the order of a single Monte-Carlo
    iteration.
    
    As a side effect, the result is also more precise since the Monte-Carlo
    method only approaches the exact result in the limit when the number of
    iterations grows towards infinity.

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -190,6 +190,18 @@ exports["default"] = calculateStrategies;
 
 var _aux = require("./aux");
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function evaluateStrategy(values, turns, exploitStart) {
   var total = 0;
   var max = null;
@@ -209,8 +221,7 @@ function evaluateStrategy(values, turns, exploitStart) {
   return total;
 }
 
-function calculateStrategies(values, turns) {
-  var iterations = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1000000;
+function calculateStrategiesMonteCarlo(values, turns, iterations) {
   var strategies = Array(turns).fill(0);
 
   for (var i = 0; i < iterations; i += 1) {
@@ -226,6 +237,60 @@ function calculateStrategies(values, turns) {
   }
 
   return strategies;
+}
+
+function calculateStrategiesDeterministically(values, turns, iterations) {
+  var strategies = Array(turns).fill(0); // We need the sorted values for the exploit phase ...
+
+  var valuesSorted = _toConsumableArray(values).sort(function (a, b) {
+    return a - b;
+  }); // ... and the average for the explore phase.
+
+
+  var average = values.reduce(function (a, b) {
+    return a + b;
+  }, 0) / values.length;
+
+  for (var exploitStart = 1; exploitStart <= turns; exploitStart += 1) {
+    // During the explore phase, we pick from the hidden values at random.
+    // The expected value of a pick is the average of all values.
+    // Multiply by the number of picks to get the total value for the explore phase.
+    var exploreTotal = average * exploitStart; // During the exploit phase, we pick the maximum value known so far.
+    // To determine the expected value, we need to compute the likelihood of each value
+    // to be the maximum in a group of `exploitStart` randomly chosen elements and multiply
+    // the likelihood by the value itself.
+    // Consider the sorted list of values `valuesSorted`: The likelihood of value `i` being
+    // the maximum is the number of combinations of size `exploitStart - 1` from the `i-1`
+    // elements that are smaller than or equal to `valuesSorted[i]` (=binomial coefficient
+    // (i - 1, exploitStart - 1)) divided by the total number of possible
+    // combinations of size `exploitStart` from the (sorted) values (=binomial coefficient
+    // (valuesSorted.length, exploitStart)).
+    //
+    // The binomial coefficient (n + 1, k) equals (n + 1) / (n + 1 - k) * (n, k).
+    // Therefore, each required binomial coefficient can be computed from the previous one
+    // resulting in the linear time algorithm below.
+
+    var pickedAsMax = 1;
+    var totalPicks = 0;
+    var exploitTotal = 0;
+
+    for (var indexOfMax = exploitStart - 1; indexOfMax < valuesSorted.length; indexOfMax += 1) {
+      exploitTotal += pickedAsMax * valuesSorted[indexOfMax];
+      totalPicks += pickedAsMax;
+      pickedAsMax = pickedAsMax * (indexOfMax + 1) / (indexOfMax + 1 - (exploitStart - 1));
+    }
+
+    exploitTotal = (turns - exploitStart) * (exploitTotal / totalPicks); // The score for the strategy is the sum of the expected values for the explore and exploit phases.
+
+    strategies[exploitStart - 1] = exploreTotal + exploitTotal;
+  }
+
+  return strategies;
+}
+
+function calculateStrategies(values, turns) {
+  var iterations = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1000000;
+  return calculateStrategiesDeterministically(values, turns, iterations);
 }
 
 },{"./aux":2}],5:[function(require,module,exports){
--- src/js/helpers/sumory-strategy.js
+++ src/js/helpers/sumory-strategy.js
@@ -18,7 +18,7 @@ function evaluateStrategy(values, turns, exploitStart) {
   return total;
 }
 
-export default function calculateStrategies(values, turns, iterations = 1000000) {
+function calculateStrategiesMonteCarlo(values, turns, iterations) {
   const strategies = Array(turns).fill(0);
 
   for (let i = 0; i < iterations; i += 1) {
@@ -32,4 +32,54 @@ export default function calculateStrategies(values, turns, iterations = 1000000)
     strategies[l] /= iterations;
   }
   return strategies;
+}
+
+function calculateStrategiesDeterministically(values, turns, iterations) {
+  const strategies = Array(turns).fill(0);
+
+  // We need the sorted values for the exploit phase ...
+  const valuesSorted = [...values].sort((a, b) => a - b);
+  // ... and the average for the explore phase.
+  const average = values.reduce((a, b) => a + b, 0) / values.length;
+
+  for (let exploitStart = 1; exploitStart <= turns; exploitStart += 1) {
+    // During the explore phase, we pick from the hidden values at random.
+    // The expected value of a pick is the average of all values.
+    // Multiply by the number of picks to get the total value for the explore phase.
+    const exploreTotal = average * exploitStart;
+
+    // During the exploit phase, we pick the maximum value known so far.
+    // To determine the expected value, we need to compute the likelihood of each value
+    // to be the maximum in a group of `exploitStart` randomly chosen elements and multiply
+    // the likelihood by the value itself.
+    // Consider the sorted list of values `valuesSorted`: The likelihood of value `i` being
+    // the maximum is the number of combinations of size `exploitStart - 1` from the `i-1`
+    // elements that are smaller than or equal to `valuesSorted[i]` (=binomial coefficient
+    // (i - 1, exploitStart - 1)) divided by the total number of possible
+    // combinations of size `exploitStart` from the (sorted) values (=binomial coefficient
+    // (valuesSorted.length, exploitStart)).
+    //
+    // The binomial coefficient (n + 1, k) equals (n + 1) / (n + 1 - k) * (n, k).
+    // Therefore, each required binomial coefficient can be computed from the previous one
+    // resulting in the linear time algorithm below.
+    let pickedAsMax = 1;
+    let totalPicks = 0;
+    let exploitTotal = 0;
+
+    for (let indexOfMax = exploitStart - 1; indexOfMax < valuesSorted.length; indexOfMax += 1) {
+      exploitTotal += pickedAsMax * valuesSorted[indexOfMax];
+      totalPicks += pickedAsMax;
+      pickedAsMax = (pickedAsMax * (indexOfMax + 1)) / (indexOfMax + 1 - (exploitStart - 1));
+    }
+    exploitTotal = (turns - exploitStart) * (exploitTotal / totalPicks);
+
+    // The score for the strategy is the sum of the expected values for the explore and exploit phases.
+    strategies[exploitStart - 1] = exploreTotal + exploitTotal;
+  }
+
+  return strategies;
+}
+
+export default function calculateStrategies(values, turns, iterations = 1000000) {
+  return calculateStrategiesDeterministically(values, turns, iterations);
 }
